2aa957d071d45bfeb303b607c8f669cae8bc8a1c
dltmdcks98 <dltmdcks98@gmail.com>
2022-04-25 21:13:10 +0900

    롬복과 리팩터링 https://tarry-helenium-836.notion.site/7-8ccf4347594d4897a8a1c005affa05cf

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j//로깅을 위한 어노테이션
 public class ArticleController {
     @Autowired//스프링 부트가 미리 생성해놓은 객체를 가져다가 자동 연결 별도의 객체생성을 안해도 됨
     private ArticleRepository articleRepository;
@@ -19,13 +21,17 @@ public class ArticleController {
 
     @PostMapping("/articles/create")//new.mustache에서 method방식이 post이므로 PostMapping으로 받아야함 인자변수에는 action부분
     public String createArticle(ArticleForm form){//클라이언트에서 입력된 데이터가 AritcleForm 파라미터로 던져진다.
-        System.out.println(form.toString());
+//        System.out.println(form.toString()); -> 로깅으로 대체
+        log.info(form.toString());
 
         Article article = form.toEntity();
-        System.out.println(article.toString());
+//        System.out.println(article.toString());
+        log.info(article.toString());
 
        Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+//        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
+
     }
 }
